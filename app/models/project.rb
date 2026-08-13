b9b20d213af3094f2be6611e85d127376a70f566
zyuumonziyari <sou0921@fuji.waseda.jp>
2026-08-13 09:53:22 +0900

    Projectモデルのバリデーション

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,19 @@
 class Project < ApplicationRecord
+  # エリアの選択肢リストを定数として定義
+  AREAS = [
+    "奥多摩・秩父",
+    "丹沢・箱根",
+    "八ヶ岳",
+    "北アルプス",
+    "南アルプス",
+    "中央アルプス",
+    "富士山",
+    "北海道"
+  ].freeze
+
+  # バリデーション（必須項目チェック）
+  validates :title, presence: true
+  validates :area, presence: true, inclusion: { in: AREAS }
+  validates :event_date, presence: true
+  validates :description, presence: true
 end
