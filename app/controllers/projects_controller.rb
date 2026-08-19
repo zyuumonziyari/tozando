428caadb7ea92233c6ef5d96c10c4bbc95cfc1ab
zyuumonziyari <sou0921@fuji.waseda.jp>
2026-08-19 13:39:46 +0900

    詳細ページから応募元ページに移行するボタンの作成

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -70,6 +70,6 @@ class ProjectsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def project_params
-      params.require(:project).permit(:title, :description, :area, :image, :apply_url)
+      params.require(:project).permit(:title, :description, :area, :image, :apply_url, :event_date)
     end
 end
