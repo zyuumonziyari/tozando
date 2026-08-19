79fa65e59e6962e68420bd12a11dcbaf3e51ecb1
zyuumonziyari <sou0921@fuji.waseda.jp>
2026-08-19 13:16:21 +0900

    フォームにリンク欄を追加

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -70,6 +70,6 @@ class ProjectsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def project_params
-      params.require(:project).permit(:title, :area, :event_date, :description, :image)
+      params.require(:project).permit(:title, :description, :area, :image, :apply_url)
     end
 end
